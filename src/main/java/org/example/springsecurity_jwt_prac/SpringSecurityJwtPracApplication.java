15f4a3a7ab16caf8f03a7174315cf24b26847040
junhyun <erik1201@naver.com>
2024-01-10 16:22:59 +0900

    create practice project

---
+++ src/main/java/org/example/springsecurity_jwt_prac/SpringSecurityJwtPracApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecurity_jwt_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springsecurity_jwt_prac/SpringSecurityJwtPracApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecurity_jwt_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
