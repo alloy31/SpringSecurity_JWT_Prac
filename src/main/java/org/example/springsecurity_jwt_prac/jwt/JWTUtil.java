84c7bcbecc32229b49175511ec63ebcec4278e3e
안준현 <erik1201@naver.com>
2024-01-19 17:29:02 +0900

    UPDATE Value annotation

--- src/main/java/org/example/springsecurity_jwt_prac/jwt/JWTUtil.java
+++ src/main/java/org/example/springsecurity_jwt_prac/jwt/JWTUtil.java
@@ -1,7 +1,7 @@
 package org.example.springsecurity_jwt_prac.jwt;
 
 import io.jsonwebtoken.Jwts;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -15,7 +15,7 @@ public class JWTUtil {
     //내부에 미리 저장해둔 SecretKey 생성
     private SecretKey secretKey;
 
-    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
 
         //객체변수로 암호화
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
